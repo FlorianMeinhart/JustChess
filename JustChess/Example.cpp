de5cb45e7d5c6c9991da020513aff5e623350ea0
florianmeinhart <florianmeinhart@gmx.at>
2020-12-25 20:18:41 +0100

    added example class and doxyfile

---
+++ JustChess/Example.cpp
@@ -0,0 +1,14 @@
+#include "Example.h"
+
+void CExample::DoSomething()
+{
+}
+
+void CExample::DoNothing()
+{
+}
+
+void* CExample::VeryUsefulMethod(bool level)
+{
+  return nullptr;
+}
---
+++ JustChess/Example.h
@@ -0,0 +1,71 @@
+#pragma once
+
+/**
+ * \class CExample
+ *
+ * \brief Provide an example
+ *
+ * This class is meant as an example.  It is not useful by itself
+ * rather its usefulness is only a function of how much it helps
+ * the reader.  It is in a sense defined by the person who reads it
+ * and otherwise does not exist in any real form.
+ *
+ * \note Attempts at zen rarely work.
+ *
+ * \author (last to touch it) $Author: bv $
+ *
+ * \version $Revision: 1.5 $
+ *
+ * \date $Date: 2005/04/14 14:16:20 $
+ *
+ * Contact: bv@bnl.gov
+ *
+ * Created on: Wed Apr 13 18:39:37 2005
+ *
+ * $Id: doxygen-howto.html,v 1.5 2005/04/14 14:16:20 bv Exp $
+ *
+ */
+
+#ifndef CExample_H
+#define CExample_H
+
+class CExample
+{
+public:
+
+  /// Constructor
+  CExample()
+    : m_question(nullptr)
+    , m_answer()
+  {};
+
+  /// Destructor
+  ~CExample() = default;
+
+  /// This method does something
+  void DoSomething();
+
+  /** This is a method that does so
+    * much that I must write an epic
+    * novel just to describe how much
+    * it truly does. */
+  void DoNothing();
+
+  /** \brief A useful method.
+    * \param level an integer setting how useful to be
+    * \return Output that is extra useful
+    *
+    * This method does unbelievably useful things.
+    * And returns exceptionally useful results.
+    * Use it everyday with good health.
+    */
+  void* VeryUsefulMethod(bool level);
+
+private:
+
+  const char* m_question; ///< the question
+  int m_answer;           ///< the answer 
+
+};                              // end of class CExample
+
+#endif  // CExample_H
